f587ef889e4104e97e2e81e56a90b4d689bb23ee
Adam Blake <adam@ablake.dev>
2023-10-06 03:17:07 -0700

    refactor: rename test_fit -> fit_stats; add tests
    #CKR-80

--- R/test_fit.R
+++ R/test_fit.R
@@ -1,5 +1,4 @@
 #' Split data into train and test sets.
-#'
 #' @param data A data frame.
 #' @param prop The proportion of rows to assign to the training set.
 #' @return A list with two data frames, `train` and `test`.
@@ -11,30 +10,20 @@ split_data <- function(data, prop = .7) {
 }
 
 #' Test the fit of a model on a train and test set.
-#'
-#' @param model An [`lm`] model (or anything compatible with [`formula()`]).
+#' @param model An [`lm`] model.
 #' @param df_train A data frame with the training data.
 #' @param df_test A data frame with the test data.
 #' @return A data frame with the fit statistics.
 #' @export
-test_fit <- function(model, df_train, df_test) {
+fit_stats <- function(model, df_train, df_test) {
   fit_train <- stats::update(model, data = df_train)
   fit_test  <- stats::update(model, data = df_test)
-  predictor <- m_predictor(model)
+  predictor <- as.character(rlang::f_lhs(stats::formula(model)))
 
   tibble::tibble(
-    data = c(
-      'train',
-      'test'
-    ),
-    F = c(
-      coursekata::f(fit_train),
-      coursekata::f(fit_test)
-    ),
-    PRE = c(
-      coursekata::pre(fit_train),
-      coursekata::pre(fit_test)
-    ),
+    data = c("train", "test"),
+    F = c(f(fit_train), f(fit_test)),
+    PRE = c(pre(fit_train), pre(fit_test)),
     RMSE = c(
       Metrics::rmse(df_train[[predictor]], stats::predict(fit_train)),
       Metrics::rmse(df_test[[predictor]], stats::predict(fit_test))
@@ -42,12 +31,6 @@ test_fit <- function(model, df_train, df_test) {
   )
 }
 
-#' Get the predictor variable from a model.
-#'
-#' @param model An [`lm`] model (or anything compatible with [`formula()`]).
-#' @return A string with the name of the predictor variable.
-#' @keywords internal
-m_predictor <- function(model) {
-  as.character(rlang::f_lhs(stats::formula(model)))
-}
-
+#' @rdname fit_stats
+#' @export
+fitstats <- fit_stats
