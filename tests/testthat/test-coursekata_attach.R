74d7dc83dc93b277c32c1197b443bf58fa58ebf0
Adam Blake <adam@ablake.dev>
2024-07-30 09:42:12 -0700

    fix: remove test dependency on suggested package

--- tests/testthat/test-coursekata_attach.R
+++ tests/testthat/test-coursekata_attach.R
@@ -1,16 +1,16 @@
 test_that("all course packages are listed with version and whether attached", {
   # these packages are not imported, but are still in the load out
   # (important because they can be detached and re-attached during testing)
-  pkgs <- c("fivethirtyeightdata", "fivethirtyeight", "Lock5withR")
+  detachable <- c("fivethirtyeightdata", "Lock5withR")
 
   # they need to be in the order they appear in the coursekata_pkgs object
-  pkgs <- pkgs[order(match(pkgs, coursekata_pkgs))]
+  detachable <- detachable[order(match(detachable, coursekata_pkgs))]
 
   # detach/re-attach only installed packages
-  installed <- pkg_is_installed(pkgs)
-  names(installed) <- pkgs
-  detacher(pkgs[installed])
-  withr::defer(attacher(pkgs[installed]))
+  installed <- pkg_is_installed(detachable)
+  names(installed) <- detachable
+  detacher(detachable[installed])
+  withr::defer(attacher(detachable[installed]))
 
   # only the installed package will be attached
   attachments <- coursekata_attach(quietly = TRUE)
